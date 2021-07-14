046c99d14d558c198191f89dcd695f0f0f8b45b4
Francyelle Moreira <francyellem64@gmail.com>
2021-07-14 09:35:15 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles.scss'
+
+export function App() {
+  return (
+    <>
+    <div className="Header">
+      <button>voltar</button>
+      <h1>Como podemos ajudar?</h1>
+    </div>
+    </>
+  );
+}
